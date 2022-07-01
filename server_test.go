de9c9b977b8082dcd828f6bf6f815b555d191d44
MIIIG <arthur.badretdinov@gmail.com>
2022-07-01 13:18:55 +0500

    Update server_test.go
    need some mock

--- server_test.go
+++ server_test.go
@@ -4,6 +4,9 @@ import "testing"
 
 func TestHandler(t *testing.T) {
 	expectedHandlerResult := "somthng"
+	if actualHandlerResult := Handler(mock1, mock2); actualHandlerResult != expectedHandlerResult {
+		t.Errorf("expected %s; got: %s", expectedHandlerResult, &actualHandlerResult)
+	}
 }
 
 // // файл foo_test.go
